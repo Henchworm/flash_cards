e6193f7f03259dadf5fdb0a6f0e7e5512916a4ca
Chris Hewitt <agop5134@gmail.com>
2021-10-13 17:56:12 -0600

    Major refactor turn.number_correct methods and spec

--- lib/round.rb
+++ lib/round.rb
@@ -1,15 +1,11 @@
 class Round
   attr_reader :deck,
-              :correct_counter,
-              :incorrect_counter,
               :turns,
               :string,
               :cards
   def initialize(deck)
     @deck = deck
     @turns = []
-    @correct_counter = 0
-    @incorrect_counter = 0
   end
 
   def current_card
@@ -18,26 +14,16 @@ class Round
 
   def take_turn(guess)
     @turns << Turn.new(guess, current_card)
-    @turns.rotate(1)
-    require "pry"; binding.pry
+    @turns.rotate!(1)
+    @turns.last
   end
 
   def number_correct
-    @turns.each do |turn|
-      if turn.card.answer == turn.string
-        @correct_counter += 1
-      end
+    turns.count do |turn|
+      turn.correct?
     end
   end
 
-
-  def number_incorrect
-    @turns.each do |turn|
-      if turn.card.answer != turn.string
-        @incorrect_counter += 1
-      end
-    end
-  end
 
   def number_correct_by_category()
 
