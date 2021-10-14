17ac62eaacaa287fced2471daba1ba8dd6731a13
Chris Hewitt <agop5134@gmail.com>
2021-10-13 20:23:06 -0600

    Create percent_by_category spec, current_card passing

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -86,6 +86,16 @@ RSpec.describe Round do
     expect(round.percent_correct).to eq(50.0)
   end
 
+  it "percent_by_category" do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+    new_turn = round.take_turn("Venus")
+    expect(round.current_card).to eq(card_3)
+  end
 
 
 end
