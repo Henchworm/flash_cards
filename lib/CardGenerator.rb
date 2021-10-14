b77be0679285ee2e83cea21045132f861581cd5d
Chris Hewitt <agop5134@gmail.com>
2021-10-14 16:41:28 -0600

    Continue CardGenerator buildout

--- lib/CardGenerator.rb
+++ lib/CardGenerator.rb
@@ -4,19 +4,23 @@ require './lib/turn'
 require './lib/card'
 
 class CardGenerator
-  attr_reader :filename
+  attr_reader :filename,
+              :cards
   def initialize(filename)
     @filename = filename
+    @cards = []
   end
 
-  def cards
+  def card_reader
     cards_data = File.open(filename)
     read_cards_data = cards_data.readlines.map(&:chomp)
     new_card = []
     read_cards_data.each do |line|
-      line.split(",")
-      line.to_array
-      require "pry"; binding.pry
+      card_array = line.split(/, /)
+      question = card_array[0]
+      answer = card_array[1]
+      category = card_array[2]
     end
+    @cards << Card.new(question, answer, category)
   end
 end
